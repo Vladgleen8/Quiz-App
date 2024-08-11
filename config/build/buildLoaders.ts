f6e1c2a8cc155091081aed457f385e44e1cc22e5
Vladis <vladis.dev@gmail.com>
2024-08-11 10:06:49 +0300

    refactor webpack config

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,11 @@
+import webpack from "webpack";
+
+const typescriptLoader = {
+  test: /\.tsx?$/,
+  use: "ts-loader",
+  exclude: /node_modules/,
+};
+
+export function buildLoaders(): webpack.RuleSetRule[] {
+  return [typescriptLoader];
+}
