19a898a6f8034058a33a742a27ac4fd616f7e8f7
Vladis <vladis.dev@gmail.com>
2024-08-11 22:39:44 +0300

    Add scss loader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -6,6 +6,18 @@ const typescriptLoader = {
   exclude: /node_modules/,
 };
 
+const cssLoader = {
+  test: /\.s[ac]ss$/i,
+  use: [
+    // Creates `style` nodes from JS strings
+    "style-loader",
+    // Translates CSS into CommonJS
+    "css-loader",
+    // Compiles Sass to CSS
+    "sass-loader",
+  ],
+};
+
 export function buildLoaders(): webpack.RuleSetRule[] {
-  return [typescriptLoader];
+  return [typescriptLoader, cssLoader];
 }
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import { render } from "react-dom";
+
+render(
+    <div>ABBdsfsdfsA</div>,
+    document.getElementById("root")
+) 
