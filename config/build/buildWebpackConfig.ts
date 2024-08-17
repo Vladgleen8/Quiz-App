8d43acf5fddc3d55cf9d9a2934ec6ffec5c397d5
Vladis <vladis.dev@gmail.com>
2024-08-17 14:41:57 +0300

    Refactor file structure for FSD use

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -22,7 +22,7 @@ export function buidWebpackConfig(
     module: {
       rules: buildLoaders(options),
     },
-    resolve: buildResolvers(),
+    resolve: buildResolvers(options),
     devtool: isDev ? "inline-source-map" : undefined,
     devServer: isDev ? buildDevServer(options) : undefined,
   };
